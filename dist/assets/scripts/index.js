2e0a3afd096b875a072d63ba9b93194a07f204f7
liwenwen <liwenwen@oak-inc.com>
2018-06-01 18:08:17 +0800

    还没写完

--- dist/assets/scripts/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ww on 2018/5/28.
- */
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
-})
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
  */
 const argv = require('yargs-parser')(process.argv.slice(2));
 const marge = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const glob = require('glob');
 const files = glob.sync('./src/webapp/views/**/*.entry.js');
 const _mode = argv.mode || "development";
@@ -25,9 +26,28 @@ for(let item of files){
 let webpackConfig = {
     entry: _entry,
     output: {
-        path: join(__dirname,'./dist/assets'),
+        path: join(__dirname,'./dist/'),
         publicPath: '/',
-        filename: 'scripts/[name].bundle.js'
-    }
+        filename: 'assets/scripts/[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin(
+            [{ from: 'src/webapp/views/', to: 'views/'}],
+            { copyUnmodified: true }
+        )
+    ]
+
 }
 module.exports = marge(_mergeConfig,webpackConfig);
